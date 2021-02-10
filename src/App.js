e0c862fdc11993d4867144cc7d797c966ec32dd0
Renon Galamgam <rgalamgam@brightedge.com>
2021-02-11 00:10:55 +1300

    fixed conflicts

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
-<<<<<<< HEAD
-=======
-import logo from './logo.svg';
->>>>>>> f0fa50777821d4b165b6f709daefa73f9b6a8000
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-<<<<<<< HEAD
         Hello Amplify!
-=======
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
->>>>>>> f0fa50777821d4b165b6f709daefa73f9b6a8000
       </header>
     </div>
   );
